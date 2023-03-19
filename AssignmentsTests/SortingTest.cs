bb88b660f0212d3a08b3a655ab5decd88b1ff956
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2023-03-19 16:42:52 +0200

    Insertion Sort

--- AssignmentsTests/SortingTest.cs
+++ AssignmentsTests/SortingTest.cs
@@ -37,5 +37,19 @@ namespace AssignmentsTests
                 Assert.Equal(expected[i], result[i]);
             }
         }
+
+        [Fact]
+        public void InsertionSort_SortArrayElements_InAscendingOrder()
+        {
+            var input = new[] { 1, 3, 8, 7, 9, 2, 8 };
+            var expected = new int[] { 1, 2, 3, 7, 8, 8, 9 };
+            var sut = new Sorting();
+            var result = sut.InsertionSort(input);
+
+            for (var i = 0; i < result.Length; i++)
+            {
+                Assert.Equal(expected[i], result[i]);
+            }
+        }
     }
 }
