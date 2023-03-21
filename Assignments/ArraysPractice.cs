72ccde42a6adb0c32f52d11ef8a1a37c397decba
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2023-03-21 13:08:12 +0200

    merge 2 sorted arrays

---
+++ Assignments/ArraysPractice.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    public class ArraysPractice
+    {
+        /// <summary>
+        /// Merge 2 ordered arrays
+        /// T.C => O(n)
+        /// S.C => O(m + n)
+        /// </summary>
+        /// <param name="leftArr"></param>
+        /// <param name="rightArr"></param>
+        /// <returns></returns>
+        public int[] MergeTwoOrderedArrays(int[] leftArr, int[] rightArr)
+        {
+            var result = new int[leftArr.Length + rightArr.Length];
+            var currentIndex = 0;
+            var leftArrIndex = 0;
+            var rightArrIndex = 0;
+
+            while (leftArrIndex < leftArr.Length && rightArrIndex < rightArr.Length)
+            {
+                if (leftArr[leftArrIndex] < rightArr[rightArrIndex])
+                {
+                    result[currentIndex] = leftArr[leftArrIndex];
+                    leftArrIndex++;
+                }
+                else
+                {
+                    result[currentIndex] = rightArr[rightArrIndex];
+                    rightArrIndex++;
+                }
+
+                currentIndex++;
+            }
+
+            while (leftArrIndex < leftArr.Length)
+            {
+                result[currentIndex] = leftArr[leftArrIndex];
+                leftArrIndex++;
+                currentIndex++;
+            }
+
+            while (rightArrIndex < rightArr.LongLength)
+            {
+                result[currentIndex] = rightArr[rightArrIndex];
+                rightArrIndex++;
+                currentIndex++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AssignmentsTests/ArraysPracticeTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignments;
+
+namespace AssignmentsTests
+{
+    public class ArraysPracticeTest
+    {
+        [Theory]
+        [InlineData(
+            new [] {1, 2, 3, 8, 11, 18}, 
+            new[] {1, 5, 28},
+            new[] {1, 1, 2, 3, 5, 8, 11, 18, 28})]
+        public static void MergeTwoOrderedArrays_ShouldReturn_AnAscendingOrderedArray_For_TwoGivenArrays(int[] input1, int[] input2, int[] expected)
+        {
+            var sut = new ArraysPractice();
+            var result = sut.MergeTwoOrderedArrays(input1, input2);
+
+            for(var i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], result[i]);
+            }
+        }
+    }
+}
