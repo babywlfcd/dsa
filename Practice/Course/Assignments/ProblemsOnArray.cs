9cd7e0bb8850a764232bd1240131bd909113ff62
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2023-03-28 16:13:13 +0300

    remove unused namespaces

--- Practice/Course/Assignments/ProblemsOnArray.cs
+++ Practice/Course/Assignments/ProblemsOnArray.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualBasic;
-
-namespace Practice.Course.Assignments
+﻿namespace Practice.Course.Assignments
 {
     public class ProblemsOnArray
     {
