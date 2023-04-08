ea6d228f6207ae310797ee62024e4b915058b2a3
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2023-04-09 00:09:10 +0300

    doubly linked list - implement add methods @head, @tail @ given posittion

---
+++ Practice/LinkedLists/DoublyNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.LinkedLists
+{
+    public class DoublyNode
+    {
+        public int Value;
+        public DoublyNode Next;
+        public DoublyNode Previous;
+
+        public DoublyNode(int value, DoublyNode previous = null, DoublyNode next = null)
+        {
+            Value = value;
+            Next = next;
+            Previous = previous;
+        }
+    }
+}
