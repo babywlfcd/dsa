efffa9a5b2d36214e48ed3686363cbd2dce140bc
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2023-04-17 10:56:38 +0300

    tree implementation

---
+++ Practice/Tree/BinaryTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Tree
+{
+    public class BinaryTree
+    {
+        public BinaryNode Root;
+
+        public BinaryTree(BinaryNode root = null)
+        {
+            Root = root;
+        }
+
+        
+    }
+}
---
+++ Practice/Tree/TreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Tree
+{
+    public class TreeNode
+    {
+        public int? Value;
+        public TreeNode[] Children;
+
+        public TreeNode(int? val, TreeNode[] children = null)
+        {
+            Value = val;
+            Children = children;
+        }
+    }
+}
