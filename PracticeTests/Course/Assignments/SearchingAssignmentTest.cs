972e243eac49dee1c75045cda6f53448d0a0857f
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2023-04-19 23:45:57 +0300

    binary implementation in class - we 3

---
+++ PracticeTests/Course/Assignments/SearchingAssignmentTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Practice.Course.Assignments;
+
+namespace PracticeTests.Course.Assignments
+{
+    public class SearchingAssignmentTest
+    {
+        [Theory]
+        [InlineData(new[] {1}, 0)]
+        [InlineData(new []{5, 4, 3, 2, 1}, 0)]
+        [InlineData(new[] { 1, 2, 3, 4, 5 }, 4)]
+        public void FindPickElement_ShouldReturn_IndexOfAnyElementTharIsGreaterThanItdNeighbors_For_AGivenArray(
+            int[] input, int expected)
+        {
+            var sut = new SearchingAssignment();
+            var acctual = sut.FindPickElement(input);
+            Assert.Equal(expected, acctual);
+        }
+    }
+}
