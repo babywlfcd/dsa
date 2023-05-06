3b2e765e3b76b03ccdd38e8d7acd2f6820925b8d
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2023-05-06 19:28:54 +0300

    trees assignment

--- Practice/Tree/Node.cs
+++ Practice/Tree/Node.cs
@@ -11,10 +11,10 @@ namespace Practice.Tree
     /// </summary>
     public class Node
     {
-        public int? Value;
+        public int Value;
         public Node Left;
         public Node Right;
-        public Node(int? value = 0, Node left = null, Node right = null)
+        public Node(int value = 0, Node left = null, Node right = null)
         {
             Value = value;
             Left = left;
