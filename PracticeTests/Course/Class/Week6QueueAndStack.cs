f9267dc4388e592a5b75a69039856262c75c8f2f
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2023-12-10 06:17:05 +0200

    merge with main

--- PracticeTests/Course/Class/Week6QueueAndStack.cs
+++ PracticeTests/Course/Class/Week6QueueAndStack.cs
@@ -12,8 +12,8 @@ namespace PracticeTests.Course.Class
         //design a data structure where we can perform push/pop/pick/is empty/getmin in O(1)
         public int Push(int value)
         {
-            var stackoriginal = new Stack1(10);
-            var minStack = new Stack1(10);
+            var stackoriginal = new StackA(10);
+            var minStack = new StackA(10);
             if (minStack.IsEmpty())
             {
                 minStack.Push(value);
@@ -27,8 +27,8 @@ namespace PracticeTests.Course.Class
 
         public int Pop(int value)
         {
-            var stackoriginal = new Stack1(10);
-            var minStack = new Stack1(10);
+            var stackoriginal = new StackA(10);
+            var minStack = new StackA(10);
             var x = stackoriginal.Pop();
 
             return 0;
