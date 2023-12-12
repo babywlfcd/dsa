d7684b0775323c37b9b32a5a2eca4ee193ff65fe
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2023-12-12 20:31:37 +0200

    review and implement problems from base recursion class

--- Practice/Course/Assignments/RecursionBasics.cs
+++ Practice/Course/Assignments/RecursionBasics.cs
@@ -3,6 +3,95 @@
     public class RecursionBasics
     {
         /*
+         * ----------------
+         * || Assignment ||       
+         * ----------------
+         */
+
+        // Q1. Sum of Digits!
+        // Initial solution
+        public int SumDigits(int n)
+        {
+
+            //var last_digit = n % 10;
+            int sum = 0;
+            while (n != 0)
+            {
+                var last_digit = n % 10;
+                sum += last_digit;
+                n = (n - last_digit) / 10;
+
+            }
+
+            return sum;
+        }
+
+        // Q1. Sum of Digits!
+        // solved with while - 1 min
+        // solved with recursion - check solution and ask
+        // Solution complexity:
+        //  10^(d-1) <= n < 10^d , d - digits of n
+        //  d-1 <= log(n) < d
+        //  d <= log(n) + 1
+        // lower bound log(n); upper bound log(n) + 1
+        // => time complexity O(log(n))
+        //    space complexity O(log(n)) - recursion
+        public int SumOfDigits(int n)
+        {
+            if (n <= 9)
+                return n;
+
+            return n % 10 + SumOfDigits(n / 10);
+        }
+
+        // Q2. Is magic?
+        // Solution time 5 min
+        // Complexity:
+        // Time and space: O(log(n))
+        public bool IsMagic(int n)
+        {
+            // preview problem return sum of digits for a given number;
+            int sum = SumOfDigits(n);
+
+            while (sum >= 10) // Remark: correct code by unit testing. Initially, the while condition was missing
+                sum = sum % 10 + SumOfDigits(sum / 10);
+
+            return sum == 1;
+        }
+
+        // Q3. Implement Power Function - pow(A, B) % C
+        // Solution time 15 min - I looked for the idea
+        // Time amd Space complexity - O(log(k))
+        public int CalculateExpressionRec(int a, int b, int c)
+        {
+            if (a < 0)
+                a = (-1) * a;
+
+            // base case scenario
+            if (b == 0)
+                return 1 % c;
+
+            // review the class negative power edge case
+            if (b < 0)
+                return 1 / CalculateExpressionRec(a, 0 - b, c); // this is always 0 ?
+                                                                // a^b = a^(b/2) * a^(b/2) - will work only for even power
+            var x = CalculateExpressionRec(a, b / 2, c);
+            if (b % 2 == 0)
+            {
+               
+                return (x * x) % c;
+            }
+            else
+            {
+               // var x = CalculateExpressionRec(a, b / 2, c);
+                return (a * x * x) % c;
+            }
+        }
+
+        // TODO: 4) Tower of hanoi - I understand the problem. Not sure about the implementation. 
+        // TODO: 5) Gray Code - I need clarification to understand the ask
+
+        /*
          * --------------
          * || Homework ||
          * --------------
--- PracticeTests/Course/Class/Week1RecursionBasicsTests.cs
+++ PracticeTests/Course/Class/Week1RecursionBasicsTests.cs
@@ -1,46 +1,19 @@
-﻿using Practice.Course.Class;
+﻿using Practice.Course.Assignments;
+using Practice.Course.Class;
 
 namespace PracticeTests.Course.Class
 {
     public class Week1RecursionBasicsTests
     {
         [Theory]
-        [InlineData(46, 10)]
-        [InlineData(11, 2)]
-        [InlineData(999999999, 81)]
-        [InlineData(1, 1)]
-        [InlineData(1000000000, 1)]
-        public void DigitSum_Should_SumOfDigits_For_ANumber(int a, int expected)
+        [InlineData(5, 3, 15)]
+        [InlineData(15, -3, -15)]
+        [InlineData(9-5, 3, -15)]
+        [InlineData(-5, -3, 15)]
+        public void DigitSum_Should_SumOfDigits_For_ANumber(int a, int n, int expected)
         {
             var sut = new Week1RecursionBasics();
-            var result = sut.SumDigits(a);
-            Assert.Equal(expected, result);
-        }
-
-        [Theory]
-        [InlineData(46, 10)]
-        [InlineData(11, 2)]
-        [InlineData(999999999, 81)]
-        [InlineData(1, 1)]
-        [InlineData(1000000000, 1)]
-        public void DigitSumRecursive_ShouldReturn_SumOfDigits_For_ANumber(int a, int expected)
-        {
-            var sut = new Week1RecursionBasics();
-            var result = sut.SumOfDigits(a);
-            Assert.Equal(expected, result);
-        }
-
-
-        [Theory]
-        [InlineData(83557, true)] // initially fail 
-        [InlineData(1291, false)]
-        [InlineData(999999999, false)]
-        [InlineData(1, true)]
-        [InlineData(1000000000, true)]
-        public void IsMagic_ShouldReturn_One_For_MagicNumber(int a, bool expected)
-        {
-            var sut = new Week1RecursionBasics();
-            var result = sut.IsMagic(a);
+            var result = sut.Multiply(a, n);
             Assert.Equal(expected, result);
         }
 
@@ -49,15 +22,58 @@ namespace PracticeTests.Course.Class
         [InlineData(-2, 3, 3, 2)]
         [InlineData(2, -3, 3, 0)]
         [InlineData(-5, 2, 7, 4)]
-        //[InlineData(-5, 3, 7, 5)] - why the result here is 6?
+        [InlineData(-5, 3, 7, 6)]
         [InlineData(-1000000000, 0, 1, 0)]
         [InlineData(1000000000, 1000000000, 1000000000, 0)]
         public void CalculateExpressionRec_ShouldReturn_PositiveOnly(int a, int b, int c, int expected)
         {
             var sut = new Week1RecursionBasics();
-            var result = sut.CalculateExpressionRec(a, b, c);
+            var result = sut.CalculateExpression(a, b, c);
             Assert.Equal(expected, result);
             Assert.True(result >= 0);
+        }
+
+        [Fact]
+        public void CalculateExpressionRec_ThrowException_ForDZero()
+        {
+            var sut = new Week1RecursionBasics();
+            Assert.Throws<DivideByZeroException>(() => sut.CalculateExpression(5, 8, 0));
+        }
+
+        [Fact]
+        public void CalculateExpressionRec_ExceptionMessage_DCannotBeZero()
+        {
+            var sut = new Week1RecursionBasics();
+            Action act =  () =>sut.CalculateExpression(5, 8, 0);
+            
+            //assert
+            var exception = Assert.Throws<DivideByZeroException>(act);
+            //The thrown exception can be used for even more detailed assertions.
+            Assert.Equal("d cannot be zero", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("abcdcba", 0, 6, true)]
+        [InlineData("abba", 0, 3, true)]
+        [InlineData("", 0, 0, true)]
+        [InlineData("a", 0, 0, true)]
+        [InlineData("abbc", 0, 3, false)]
+        public void IsPalindrome_ShouldValidate_AGivenString(string s, int left, int right, bool expected)
+        {
+            var sut = new Week1RecursionBasics();
+            var result = sut.IsPalindrome(s, left, right);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(25, 1, 5)]
+        [InlineData(30, 1, 5)]
+        [InlineData(1296, 1, 36)]
+        public void Sqrt_Return_SquareRoot_for_GivenNaturalNumber(int n, int k, int expected)
+        {
+            var sut = new Week1RecursionBasics();
+            var result = sut.Sqrt(n, k);
+            Assert.Equal(expected, result);
         }
     }
 }
