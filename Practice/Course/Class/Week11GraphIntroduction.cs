c7a924c987174e5ff23c3d627dc912cef234a794
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2024-10-07 14:02:11 +0300

    graph representation implementation (Adjacency Matrix, Adjacency List)

---
+++ Practice/Course/Class/Week11GraphIntroduction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Course.Class
+{
+    public class Week11GraphIntroduction
+    {
+        // Graphs are a collection of nodes and edges that connect these nodes.
+        // Graphs can be directed or undirected.
+        // Graphs can be weighted or unweighted.
+        // Graphs can be cyclic or acyclic.
+        // Graphs can be connected or disconnected.
+        // Graphs can be represented in two ways: Adjacency Matrix and Adjacency List.
+        // Adjacency Matrix is a 2D array of size V x V where V is the number of vertices in the graph.
+        // Adjacency List is an array of linked lists where the size of the array is equal to the number of vertices in the graph.
+        // The most common graph traversal algorithms are Breadth First Search (BFS) and Depth First Search (DFS).
+        // BFS is used to find the shortest path between two nodes.
+        // DFS is used to find connected components in a graph.
+        // BFS and DFS can be implemented using both Adjacency Matrix and Adjacency List.
+        // BFS and DFS have a time complexity of O(V + E) where V is the number of vertices and E is the number of edges in the graph.
+        // BFS and DFS have a space complexity of O(V) where V is the number of vertices in the graph.
+        // BFS uses a queue data structure to keep track of the nodes to visit next.
+        // DFS uses a stack data structure to keep track of the nodes to visit next.
+        // BFS and DFS can be used to solve a variety of graph problems such as finding the shortest path, detecting cycles, and finding connected components.
+        // BFS and DFS are fundamental algorithms in graph theory and are used in a wide range of applications such as social networks, routing algorithms, and recommendation systems.
+
+        // See GraphMatrix.cs for an example of how to implement a graph using an adjacency matrix.
+        // See GraphList.cs for an example of how to implement a graph using an adjacency list.
+
+    }
+}
---
+++ Practice/Graphs/GraphMatrix.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Graphs
+{
+    public class GraphMatrix
+    {
+        private readonly int _vertices;
+        private int[][] _adjacencyMatrix;
+
+        public GraphMatrix(int vertices)
+        {
+            _vertices = vertices;
+        }
+
+        public int[][] CreateAdjacencyMatrix()
+        {
+            _adjacencyMatrix = new int[_vertices + 1][];
+            for (var i = 0; i <= _vertices; i++)
+                _adjacencyMatrix[i] = new int[_vertices + 1];
+
+            
+            return _adjacencyMatrix;
+        }
+
+        public void AddEdgeUnWeightedUndirected(int source, int destination)
+        {
+            _adjacencyMatrix[source][destination] = 1;
+            _adjacencyMatrix[destination][source] = 1;
+        }
+
+        public void AddEdgeUnWeightedDirected(int source, int destination)
+        {
+            _adjacencyMatrix[source][destination] = 1;
+        }
+
+        public void AddEdgeWeightedUndirected(int source, int destination, int weight)
+        {
+            _adjacencyMatrix[source][destination] = weight;
+            _adjacencyMatrix[destination][source] = weight;
+        }
+
+        public void AddEdgeWeightedDirected(int source, int destination, int weight)
+        {
+            _adjacencyMatrix[source][destination] = weight;
+        }
+
+        public string PrintGraph()
+        {
+            var n = _adjacencyMatrix.Length;
+            var sb = new StringBuilder();
+            for (var i = 0; i < n; i++)
+            {
+                
+                for (var j = 0; j < n; j++)
+                    sb.Append($"{_adjacencyMatrix[i][j]} ");
+
+                sb.AppendLine();
+                
+            }
+            return sb.ToString();
+        }
+    }
+}
