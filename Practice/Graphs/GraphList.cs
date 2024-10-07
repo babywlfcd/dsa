e0221d59a21e01630108410b30c9df93781c1eef
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2024-10-07 14:02:11 +0300

    graph representation implementation (Adjecency Matrix, Adjecency List)

---
+++ Practice/Graphs/GraphList.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Graphs
+{
+    public class GraphList
+    {
+        private readonly int _vertices;
+        private List<List<int>> _adjacencyList;
+        private List<List<(int, int)>> _adjacencyListWheighted;
+
+        public GraphList(int vertices)
+        {
+            _vertices = vertices;
+        }
+
+        public List<List<int>> CreateAdjacencyList()
+        {
+            _adjacencyList = new List<List<int>>();
+            // if  we want to not handle 0 based index we can create a list of size vertices + 1
+            // i <= _vertices
+            for (var i = 0; i < _vertices; i++)
+            {
+                _adjacencyList.Add(new List<int>());
+            }
+
+            return _adjacencyList;
+        }
+
+        public List<List<int>> CreateAdjacencyListWheighted()
+        {
+            _adjacencyListWheighted = new List<List<(int, int)>>();
+            // if  we want to not handle 0 based index we can create a list of size vertices + 1
+            // i <= _vertices
+            for (var i = 0; i < _vertices; i++)
+            {
+                _adjacencyListWheighted.Add(new List<(int, int)>());
+            }
+
+            return _adjacencyList;
+        }
+
+        /// <summary>
+        /// if  we want to not handle 0 based index we can create a list of size vertices + 1
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        public void AddEdgeUnWeightedUndirected(int source, int destination)
+        {
+            // if  we want to not handle 0 based index we can create a list of size vertices + 1
+            // _adjacencyList[source].Add(destination);
+            // _adjacencyList[destination].Add(source);
+            _adjacencyList[source - 1].Add(destination);
+            _adjacencyList[destination - 1].Add(source);
+        }
+
+        public void AddEdgeUnWeightedDirected(int source, int destination)
+        {
+            _adjacencyList[source - 1].Add(destination);
+        }
+
+        public void AddEdgeWeightedUndirected(int source, int destination, int weight)
+        {
+            _adjacencyListWheighted[source - 1].Add((destination, weight));
+            _adjacencyListWheighted[destination - 1].Add((source, weight));
+        }
+
+        public void AddEdgeWeightedDirected(int source, int destination, int weight)
+        {
+            _adjacencyListWheighted[source - 1].Add((destination, weight));
+        }
+
+        public string PrintGraph()
+        {
+            var n = _adjacencyList.Count;
+            var sb = new StringBuilder();
+            for (var i = 0; i < n; i++)
+            {
+                sb.Append($"{i}: ");
+                foreach (var item in _adjacencyList[i])
+                    sb.Append($"{item} ");
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+        public string PrintGraphWeight()
+        {
+            var n = _adjacencyListWheighted.Count;
+            var sb = new StringBuilder();
+            for (var i = 0; i < n; i++)
+            {
+                sb.Append($"{i}: ");
+                foreach (var item in _adjacencyListWheighted[i])
+                    sb.Append($"{item} ");
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ PracticeTests/Graphs/GraphMatrixTests.cs
@@ -0,0 +1,99 @@
+﻿using Practice.Graphs;
+
+namespace PracticeTests.Graphs
+{
+    public class GraphMatrixTests
+    {
+        [Fact]
+        public void TestAdjacencyMatrix_UnweightedUndirectedGraph()
+        {
+            var graph = new GraphMatrix(5);
+            var graphMatrix = graph.CreateAdjacencyMatrix();
+            graph.AddEdgeUnWeightedUndirected(1, 2);
+            graph.AddEdgeUnWeightedUndirected(1, 4);
+            graph.AddEdgeUnWeightedUndirected(2, 3);
+            graph.AddEdgeUnWeightedUndirected(2, 5);
+            graph.AddEdgeUnWeightedUndirected(2, 4);
+            graph.AddEdgeUnWeightedUndirected(3, 5);
+            graph.AddEdgeUnWeightedUndirected(3, 4);
+
+            var result = graph.PrintGraph();
+            Assert.Equal(
+                "0 0 0 0 0 0 \r\n" +
+                "0 0 1 0 1 0 \r\n" +
+                "0 1 0 1 1 1 \r\n" +
+                "0 0 1 0 1 1 \r\n" +
+                "0 1 1 1 0 0 \r\n" +
+                "0 0 1 1 0 0 \r\n", result);
+        }
+
+        [Fact]
+        public void TestAdjacencyMatrix_UnweightedDirectedGraph()
+        {
+            var graph = new GraphMatrix(5);
+            var graphMatrix = graph.CreateAdjacencyMatrix();
+            graph.AddEdgeUnWeightedDirected(1, 2);
+            graph.AddEdgeUnWeightedDirected(1, 4);
+            graph.AddEdgeUnWeightedDirected(2, 3);
+            graph.AddEdgeUnWeightedDirected(2, 5);
+            graph.AddEdgeUnWeightedDirected(2, 4);
+            graph.AddEdgeUnWeightedDirected(3, 5);
+            graph.AddEdgeUnWeightedDirected(3, 4);
+
+            var result = graph.PrintGraph();
+            Assert.Equal(
+                "0 0 0 0 0 0 \r\n" +
+                "0 0 1 0 1 0 \r\n" +
+                "0 0 0 1 1 1 \r\n" +
+                "0 0 0 0 1 1 \r\n" +
+                "0 0 0 0 0 0 \r\n" +
+                "0 0 0 0 0 0 \r\n", result);
+        }
+
+        [Fact]
+        public void TestAdjacencyMatrix_WeightedUndirectedGraph()
+        {
+            var graph = new GraphMatrix(5);
+            var graphMatrix = graph.CreateAdjacencyMatrix();
+            graph.AddEdgeWeightedUndirected(1, 2, 12);
+            graph.AddEdgeWeightedUndirected(1, 4, 15);
+            graph.AddEdgeWeightedUndirected(2, 3, 8);
+            graph.AddEdgeWeightedUndirected(2, 5, 5);
+            graph.AddEdgeWeightedUndirected(2, 4, 10);
+            graph.AddEdgeWeightedUndirected(3, 5, 11);
+            graph.AddEdgeWeightedUndirected(3, 4, 3);
+
+            var result = graph.PrintGraph();
+            Assert.Equal(
+                "0 0 0 0 0 0 \r\n" +
+                "0 0 12 0 15 0 \r\n" +
+                "0 12 0 8 10 5 \r\n" +
+                "0 0 8 0 3 11 \r\n" +
+                "0 15 10 3 0 0 \r\n" +
+                "0 0 5 11 0 0 \r\n", result);
+        }
+
+        [Fact]
+        public void TestAdjacencyMatrix_WeightedDirectedGraph()
+        {
+            var graph = new GraphMatrix(5);
+            var graphMatrix = graph.CreateAdjacencyMatrix();
+            graph.AddEdgeWeightedDirected(1, 2, 12);
+            graph.AddEdgeWeightedDirected(1, 4, 15);
+            graph.AddEdgeWeightedDirected(2, 3, 8);
+            graph.AddEdgeWeightedDirected(2, 5, 5);
+            graph.AddEdgeWeightedDirected(2, 4, 10);
+            graph.AddEdgeWeightedDirected(3, 5, 11);
+            graph.AddEdgeWeightedDirected(3, 4, 3);
+
+            var result = graph.PrintGraph();
+            Assert.Equal(
+                "0 0 0 0 0 0 \r\n" +
+                "0 0 12 0 15 0 \r\n" +
+                "0 0 0 8 10 5 \r\n" +
+                "0 0 0 0 3 11 \r\n" +
+                "0 0 0 0 0 0 \r\n" +
+                "0 0 0 0 0 0 \r\n", result);
+        }
+    }
+}
